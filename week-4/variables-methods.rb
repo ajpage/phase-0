3e16c291c8cd7c0f533ae80ab9f445d0b3c56650
Alyssa Page <alyssa.j.page@gmail.com>
2016-01-19 12:24:38 -0600

    Add reflection

--- week-4/variables-methods.rb
+++ week-4/variables-methods.rb
@@ -1,3 +1,26 @@
+=begin
+How do you define a local variable?
+	local_var = "this is a local variable"
+
+How do you define a method?
+	def method_name (arg)
+		method content
+	end
+
+What is the difference between a local variable and a method?
+Local varibles store information that can be used at another time.
+A method does some task.
+
+How do you run a ruby program from the command line?
+	ruby file_name.rb
+
+How do you run an RSpec file from the command line?
+	rspec rspec_file_name.rb
+
+What was confusing about this material? What made sense?
+It has all just been refresher so far.
+=end
+
 #full name greeting
 puts "What is your first name?"
 first_name = gets.chomp
